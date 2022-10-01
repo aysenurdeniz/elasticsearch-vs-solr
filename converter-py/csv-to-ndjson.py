c9837849b05d5cf3c25666a0f61cbfc48a17a67c
MehdiALOMER <52455021+MehdiALOMER@users.noreply.github.com>
2022-10-01 15:46:59 +0300

    update

--- converter-py/csv-to-ndjson.py
+++ converter-py/csv-to-ndjson.py
@@ -65,3 +65,29 @@ with open("C:\\Users\\anurd\\Desktop\\dataset\\csv\\wos-papers.csv", "r") as f3:
                      + "\"}\n")
             number = number + 1
         tx.close()
+        
+with open("C:\\Users\\mehdi\\OneDrive\\Masaüstü\\dataset\\archive\\Reviews.csv", "r", encoding='utf-8') as f3:
+    reader = csv.reader(f3)
+    next(reader)
+    data = []
+    number = 1
+    st = '{ \"index\" : { \"_index\":\"customers_reviews\" , \"_id\":\"'
+    strdata = ['{\"Id\":\"', '\", \"ProductId\":\"', '\", \"UserId\":\"', '\", \"ProfileName\":\"', '\", \"HelpfulnessNumerator\":\"',
+               '\", \"HelpfulnessDenominator\":\"', '\", \"Score\":\"', '\", \"Time\":\"', '\", \"Summary\":\"',
+               '\", \"Text\":\"']
+    with open("nd_customers_reviews.json", "w", encoding='utf-8') as tx:
+        for row in reader:
+            tx.write(st + str(number) + "\" }\n")
+            tx.write(strdata[0] + str(row[0].replace('"', "'"))
+                     + strdata[1] + str(row[1].replace('"', "'"))
+                     + strdata[2] + str(row[2].replace('"', "'"))
+                     + strdata[3] + str(row[3].replace('"', "'"))
+                     + strdata[4] + str(row[4].replace('"', "'"))
+                     + strdata[5] + str(row[5].replace('"', "'"))
+                     + strdata[6] + str(row[6].replace('"', "'"))
+                     + strdata[7] + str(row[7].replace('"', "'"))
+                     + strdata[8] + str(row[8].replace('"', "'"))
+                     + strdata[9] + str(row[9].replace('"', "'"))
+                     + "\"}\n")
+            number = number + 1
+        tx.close()
